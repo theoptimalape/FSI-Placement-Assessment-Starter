38a7eabcd45cd0c88c0f98e158a6904b52a93984
Rodriguez <amador.rodriguez@zekelman.com>
2022-02-08 19:06:06 -0800

    Completed Assessment

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,77 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Amador J. Rodriguez" // HINT: Replace this with your own name!
 
-// We'll use these variables to track the counts of each cookie type
+// Variables
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
+let total = gb + cc + sugar
 
-// selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
+const gbMinusBtn = document.querySelector('#minus-gb')
 const gbPlusBtn = document.querySelector('#add-gb')
+const ccMinusBtn = document.querySelector('#minus-cc')
+const ccPlusBtn = document.querySelector('#add-cc')
+const sugarMinusBtn = document.querySelector('#minus-sugar')
+const sugarPlusBtn = document.querySelector('#add-sugar')
+const totalDis = document.querySelector('#qty-total')
+
+// Functions
+function updateGB(displayGB) {
+    let gb = document.querySelector('#qty-gb')
+    gb.innerHTML = displayGB
+    totalDis.innerHTML = total
+}
+function updateCC(displayCC) {
+    let cc = document.querySelector('#qty-cc')
+    cc.innerHTML = displayCC
+    totalDis.innerHTML = total
+}
+function updateSugar(displaySugar) {
+    let sugar = document.querySelector('#qty-sugar')
+    sugar.innerHTML = displaySugar
+    totalDis.innerHTML = total
+}
+
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
-
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+gbMinusBtn.addEventListener('click', function(e) {
+    if(gb > 0) {
+        gb--
+        total--
+    }
+    updateGB(`${gb}`)
+})
+gbPlusBtn.addEventListener('click', function () {
+    gb++
+    total++
+    updateGB(`${gb}`)
+})
+ccMinusBtn.addEventListener('click', function(e) {
+    if(cc > 0) {
+        cc--
+        total--
+    }
+    updateCC(`${cc}`)
+})
+ccPlusBtn.addEventListener('click', function () {
+    cc++
+    total++
+    updateCC(`${cc}`)
+})
+sugarMinusBtn.addEventListener('click', function(e) {
+    if(sugar > 0) {
+        sugar--
+        total--
+    }
+    updateSugar(`${sugar}`)
+})
+sugarPlusBtn.addEventListener('click', function () {
+    sugar++
+    total++
+    updateSugar(`${sugar}`)
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
